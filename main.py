0f271bb62080cd1959a53237639433c268d71010
oe-tatsuhiro-private <pri.oe.tatsuhiro@gmail.com>
2026-03-08 22:52:17 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    x = 2
+    if x > 0:
+        print("xは正の数です")
+    elif x == 0:
+        print("xは0です")
+    else:
+        print("xは負の数です")
+    print(x)
+
+    
+
+
+if __name__ == "__main__":
+    main()
